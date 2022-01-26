f9cdc5ff6a5898b2a4f87e2a2cc891d12cd1346b
Andre Silva <andre.silva@mercedes-benz.io>
2022-01-26 21:57:27 +0000

    [VW-01] creation of HeaderComponent

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,18 @@
-<script setup lang="ts">
-  defineProps<{ msg: string }>()
+<script lang="ts">
+  import Header from '@/structure/Header.vue';
+
+  export default {
+    components: {
+      Header,
+    },
+    props: {
+      msg: { type: String, default: '' },
+    },
+  }
 </script>
 
 <template>
+  <Header />
   <h1>{{ msg }}</h1>
 </template>
 
