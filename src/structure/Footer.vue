9c0e55d5ef2febd970bda0e03db3a897c3d2b8bc
Andre Silva <andre.silva@mercedes-benz.io>
2022-04-23 16:44:27 +0100

    [VW-02] implementation of footer component

---
+++ src/structure/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="footer">
+    <h1>footer Component</h1>
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  props: [],
+  mounted () {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .footer {
+
+  }
+</style>
---
+++ src/structure/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="header">
+    <h1>header Component</h1>
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  props: [],
+  mounted () {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .header {
+
+  }
+</style>
